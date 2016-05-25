10f0c8f4d06357e0252701ff0af8817c95d0c31a
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-05-25 19:50:44 +0900

    submission system button

--- app/helpers/call_for_paper_helper.rb
+++ app/helpers/call_for_paper_helper.rb
@@ -1,6 +1,6 @@
 module CallForPaperHelper
   def submission_system_button
-    link_to "Submission System", "https://precisionconference.com/~vgtc", :target=>:blank, :class=>"buton"
+    content_tag :div, (link_to (content_tag :span, "Submission System", :class=>"button"), "https://precisionconference.com/~vgtc", :target=>:blank), :class=>"submission_system_button"
   end
 
 end
