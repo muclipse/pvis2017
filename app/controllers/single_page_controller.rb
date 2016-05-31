097a3dda6f290af88ed6893352fd5a762dd6c8ee
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-05-31 18:08:15 +0900

    general conference chair display in main

--- app/controllers/single_page_controller.rb
+++ app/controllers/single_page_controller.rb
@@ -7,6 +7,9 @@ class SinglePageController < ApplicationController
     use_common_content
     
     @title = "Welcome to IEEE Pacific Visualization 2017!"
+
+    general_group = ChairGroup.find_by_name("General Conference Chairs")
+    @general_chairs = general_group.chairs unless general_group.nil?
         
     render layout: 'double_column'
   end
