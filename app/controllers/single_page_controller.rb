436f30770254af81549938534679c3cf83d18c7a
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2016-06-30 16:33:09 +0900

    venue information refactored based on Hoam website

--- app/controllers/single_page_controller.rb
+++ app/controllers/single_page_controller.rb
@@ -36,13 +36,16 @@ class SinglePageController < ApplicationController
     @routes_hoam = [
       {
         :from=>"Incheon International Airport",
-        :to=>"Nakseongdae Station",
-        :url=>"https://www.google.co.kr/maps/dir/Incheon+International+Airport,+Incheon/Nakseongdae,+Seoul/@37.5174251,126.5719698,11z/am=t/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x357b9a833a5efa59:0x8d4ba096cb5cbed4!2m2!1d126.4406957!2d37.4601908!1m5!1m1!1s0x357ca0251fc2d811:0x1bea2f567c3135ef!2m2!1d126.963418!2d37.477117?hl=en"
+        :to=>"Hoam Faculy House",
+        :description=>"Take the <strong>#6017 Airport limousine bus</strong> at the <strong>GATE 6B</strong> or <strong>13B</strong>. Get off at the <u>last stop</u> \"Hoam Faculty House\"",
+        :url=>"http://www.airportlimousine.co.kr/eng/lbr/lbr03_3_6.php",
+        :url_text=>"Airport Bus Time Table"
       },
 
       {
         :from=>"Nakseongdae Station",
         :to=>"Hoam Faculty House",
+        :url=>"https://www.google.co.kr/maps/dir/Nakseongdae/Seoul,+Gwanak-gu,+Bongcheon-dong,+239-1+%EC%84%9C%EC%9A%B8%EB%8C%80%ED%95%99%EA%B5%90+%ED%98%B8%EC%95%94%EA%B5%90%EC%88%98%ED%9A%8C%EA%B4%80/@37.4728665,126.9584086,16z/am=t/data=!3m1!4b1!4m16!4m15!1m5!1m1!1s0x357ca0251fc2d811:0x1bea2f567c3135ef!2m2!1d126.963418!2d37.477117!1m5!1m1!1s0x357ca018af295bcd:0xb748d367f0649e98!2m2!1d126.9589084!2d37.4677522!2m2!7e2!8j1467108000?hl=en",
         :description=>"Go out to the exit number 4. Take the bus 02 in front of the gas station. Then take off the bus at the 'Hoam faculty house' after about 5 minutes."
       }
 
