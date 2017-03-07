beede4525761838d388d22a94aecd94da191197c
Young-Ho Kim <yhkim@hcil.snu.ac.kr>
2017-03-07 16:50:26 +0900

    chairs in chairgroup are ordered by position.

--- app/models/chair_group.rb
+++ app/models/chair_group.rb
@@ -1,6 +1,6 @@
 class ChairGroup < ActiveRecord::Base
   has_many :chair_assignments, dependent: :destroy
-  has_many :chairs, through: :chair_assignments
+  has_many :chairs, -> { order("position ASC") }, through: :chair_assignments
 
   accepts_nested_attributes_for :chair_assignments, :allow_destroy=>true
 end
